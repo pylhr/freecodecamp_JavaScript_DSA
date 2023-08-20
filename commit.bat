a14fa91fd6aa6cad85fe1fe7e6a5a360ac9c41dc
priyanshu lohar <priyanshulohar1@gmail.com>
2023-08-20 13:26:53 +0530

    Automated commit

--- commit.bat
+++ commit.bat
@@ -1,7 +1,15 @@
 @echo off
 :loop
+
+
+REM Rename .txt files to .js
+for %%i in (*.txt) do (
+    ren "%%i" "%%~ni.js"
+)
+
 git add -A
 git commit -m "Automated commit"
 git push origin main
+
 timeout /t 3 /nobreak > nul
 goto loop
