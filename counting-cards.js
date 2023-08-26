5572feacbc4b89796f5ec08fd78c7bb9bb9aec3a
priyanshu lohar <priyanshulohar1@gmail.com>
2023-08-26 15:38:30 +0530

    Automated commit

---
+++ counting-cards.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  var regex = /[JQKA]/;
+  if (card > 1 && card < 7) {
+    count++;
+  } else if (card === 10 || regex.test(card)) {
+    count--;
+  }
+
+  if (count > 0) return count + " Bet";
+  return count + " Hold";
+
+  // Only change code above this line
+}
