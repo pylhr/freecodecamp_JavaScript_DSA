b6998c00bbed9f197bb500dc2ae89b475e0ea975
priyanshu lohar <priyanshulohar1@gmail.com>
2023-08-29 10:01:13 +0530

    Automated commit

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,23 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist" : "Kenny Rogers",
+    "title" : "The Gambler",
+    "release_year" : 1978,
+    "formats" : [
+      "CD",
+      "Cassette",
+      "LP"
+    ]
+  }
+];
